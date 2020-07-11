3953c4bb982a152bb7579c9de4f8765aef6c88b7
Vishwas verma <vermavarun932@gmail.com>
2020-07-12 00:57:22 +0530

    Updates

---
+++ precache-manifest.e7688d1054d13c6dc807a4cd7bfbae1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88507d0ee4e97ac8970d1be8a1e680e",
+    "url": "/Weather-Report/index.html"
+  },
+  {
+    "revision": "08d5b567380592bf6125",
+    "url": "/Weather-Report/static/css/main.06cee338.chunk.css"
+  },
+  {
+    "revision": "119cb7d6f2abd56980df",
+    "url": "/Weather-Report/static/js/2.a1e35ff0.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Weather-Report/static/js/2.a1e35ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d5b567380592bf6125",
+    "url": "/Weather-Report/static/js/main.3a240549.chunk.js"
+  },
+  {
+    "revision": "77c849b83875b265d46e",
+    "url": "/Weather-Report/static/js/runtime-main.9026e542.js"
+  }
+]);
